fbc4e17d91b13439c6b7f8ad8c8bba9ff6639351
Yoshitaka Totsuka <y047aka@gmail.com>
2024-08-24 21:45:01 +0900

    feat: remove old `Main` module

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,24 @@
 module Pages.Home_ exposing (page)
 
-import Html
+import Html exposing (a, br, text)
+import Html.Attributes exposing (href)
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body =
+        [ a [ href "/gap-chart" ] [ text "Gap Chart" ]
+        , br [] []
+        , a [ href "/lapTime-chart" ] [ text "LapTime Chart" ]
+        , br [] []
+        , a [ href "/lapTime-charts-by-driver" ] [ text "LapTime Charts By Driver" ]
+        , br [] []
+        , a [ href "/leaderboard" ] [ text "Leader Board" ]
+        , br [] []
+        , a [ href "/leaderboard-wec" ] [ text "Leader Board WEC" ]
+        , br [] []
+        , a [ href "/wec" ] [ text "Wec" ]
+        ]
     }
