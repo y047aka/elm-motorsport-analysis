2c600f8d57a2c7e5da881d14b1106be407dfccd3
Yoshitaka Totsuka <y047aka@gmail.com>
2024-12-03 03:27:46 +0900

    chore: `Preprocess.laps_`

--- benchmark/Preprocess/Helper/Benchmark.elm
+++ benchmark/Preprocess/Helper/Benchmark.elm
@@ -114,7 +114,7 @@ preprocess_laps_Suite =
         (\_ -> laps_deprecated options)
         "improved"
         -- 2,199 runs/s (GoF: 99.96%)
-        (\_ -> laps_improved options)
+        (\_ -> Data.Wec.Preprocess.laps_ options)
     ]
 
 
@@ -207,47 +207,6 @@ preprocess_deprecated { carNumber, laps, startPositions, ordersByLap } =
             startPositions
                 |> List.Extra.findIndex ((==) carNumber)
                 |> Maybe.withDefault 0
-
-        laps_ =
-            laps
-                |> List.indexedMap
-                    (\index { driverName, lapNumber, lapTime, s1, s2, s3, elapsed } ->
-                        { carNumber = carNumber
-                        , driver = driverName
-                        , lap = lapNumber
-                        , position =
-                            Data.Wec.Preprocess.getPositionAt { carNumber = carNumber, lapNumber = lapNumber } ordersByLap
-                        , time = lapTime
-                        , best =
-                            laps
-                                |> List.take (index + 1)
-                                |> List.map .lapTime
-                                |> List.minimum
-                                |> Maybe.withDefault 0
-                        , sector_1 = Maybe.withDefault 0 s1
-                        , sector_2 = Maybe.withDefault 0 s2
-                        , sector_3 = Maybe.withDefault 0 s3
-                        , s1_best =
-                            laps
-                                |> List.take (index + 1)
-                                |> List.filterMap .s1
-                                |> List.minimum
-                                |> Maybe.withDefault 0
-                        , s2_best =
-                            laps
-                                |> List.take (index + 1)
-                                |> List.filterMap .s2
-                                |> List.minimum
-                                |> Maybe.withDefault 0
-                        , s3_best =
-                            laps
-                                |> List.take (index + 1)
-                                |> List.filterMap .s3
-                                |> List.minimum
-                                |> Maybe.withDefault 0
-                        , elapsed = elapsed
-                        }
-                    )
     in
     { carNumber = carNumber
     , drivers = drivers
@@ -256,7 +215,12 @@ preprocess_deprecated { carNumber, laps, startPositions, ordersByLap } =
     , team = team_
     , manufacturer = manufacturer_
     , startPosition = startPosition
-    , laps = laps_
+    , laps =
+        laps_deprecated
+            { carNumber = carNumber
+            , laps = laps
+            , ordersByLap = ordersByLap
+            }
     , currentLap = Nothing
     , lastLap = Nothing
     }
@@ -316,69 +280,3 @@ laps_deprecated { carNumber, laps, ordersByLap } =
                 , elapsed = elapsed
                 }
             )
-
-
-type alias Acc =
-    { bestLapTime : Maybe Int
-    , bestS1 : Maybe Int
-    , bestS2 : Maybe Int
-    , bestS3 : Maybe Int
-    , laps : List Lap
-    }
-
-
-laps_improved :
-    { carNumber : String
-    , laps : List Wec.Lap
-    , ordersByLap : OrdersByLap
-    }
-    -> List Lap
-laps_improved { carNumber, laps, ordersByLap } =
-    let
-        step : Wec.Lap -> Acc -> Acc
-        step { driverName, lapNumber, lapTime, s1, s2, s3, elapsed } acc =
-            let
-                bestLapTime =
-                    List.minimum (lapTime :: List.filterMap identity [ acc.bestLapTime ])
-
-                ( bestS1, bestS2, bestS3 ) =
-                    ( List.minimum (List.filterMap identity [ s1, acc.bestS1 ])
-                    , List.minimum (List.filterMap identity [ s2, acc.bestS2 ])
-                    , List.minimum (List.filterMap identity [ s3, acc.bestS3 ])
-                    )
-
-                currentLap =
-                    { carNumber = carNumber
-                    , driver = driverName
-                    , lap = lapNumber
-                    , position = Data.Wec.Preprocess.getPositionAt { carNumber = carNumber, lapNumber = lapNumber } ordersByLap
-                    , time = lapTime
-                    , best = Maybe.withDefault 0 bestLapTime
-                    , sector_1 = Maybe.withDefault 0 s1
-                    , sector_2 = Maybe.withDefault 0 s2
-                    , sector_3 = Maybe.withDefault 0 s3
-                    , s1_best = Maybe.withDefault 0 bestS1
-                    , s2_best = Maybe.withDefault 0 bestS2
-                    , s3_best = Maybe.withDefault 0 bestS3
-                    , elapsed = elapsed
-                    }
-            in
-            { bestLapTime = bestLapTime
-            , bestS1 = bestS1
-            , bestS2 = bestS2
-            , bestS3 = bestS3
-            , laps = currentLap :: acc.laps
-            }
-
-        initialAcc =
-            { bestLapTime = Nothing
-            , bestS1 = Nothing
-            , bestS2 = Nothing
-            , bestS3 = Nothing
-            , laps = []
-            }
-    in
-    laps
-        |> List.foldl step initialAcc
-        |> .laps
-        |> List.reverse
