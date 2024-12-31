c61a90bb914536a21cdb2b386fb9a9dc523c91d3
Yoshitaka Totsuka <y047aka@gmail.com>
2025-01-01 01:21:46 +0900

    benchmark

--- benchmark/Fixture/Benchmark.elm
+++ benchmark/Fixture/Benchmark.elm
@@ -14,19 +14,26 @@ main =
 suite : Benchmark
 suite =
     describe "Fixture" <|
-        [ Benchmark.scale "csvDecodedOfSize"
-            ([ 5 -- 63,654,091 runs/s (GoF: 99.99%)
-             , 50 -- 6,706,644 runs/s (GoF: 99.99%)
-             , 500 -- 61,496 runs/s (GoF: 99.94%)
-             , 5000 -- 21,782 runs/s (GoF: 99.98%)
+        [ Benchmark.compare "xxxDecoded"
+            "csvDecoded"
+            -- 289,888,583 runs/s (GoF: 99.92%)
+            (\_ -> Fixture.csvDecoded)
+            "jsonDecoded"
+            -- 289,947,739 runs/s (GoF: 99.98%)
+            (\_ -> Fixture.jsonDecoded)
+        , Benchmark.scale "csvDecodedOfSize"
+            ([--    5 -- 63,654,091 runs/s (GoF: 99.99%)
+              --  , 50 -- 6,706,644 runs/s (GoF: 99.99%)
+              --  , 500 -- 61,496 runs/s (GoF: 99.94%)
+              --  , 5000 -- 21,782 runs/s (GoF: 99.98%)
              ]
                 |> List.map (\size -> ( toString size, \_ -> Fixture.csvDecodedOfSize size ))
             )
         , Benchmark.scale "jsonDecodedOfSize"
-            ([ 5 -- 63,155,588 runs/s (GoF: 100%)
-             , 50 -- 6,668,796 runs/s (GoF: 99.99%)
-             , 500 -- 62,312 runs/s (GoF: 99.96%)
-             , 5000 -- 21,906 runs/s (GoF: 99.99%)
+            ([--    5 -- 63,155,588 runs/s (GoF: 100%)
+              --  , 50 -- 6,668,796 runs/s (GoF: 99.99%)
+              --  , 500 -- 62,312 runs/s (GoF: 99.96%)
+              --  , 5000 -- 21,906 runs/s (GoF: 99.99%)
              ]
                 |> List.map (\size -> ( toString size, \_ -> Fixture.jsonDecodedOfSize size ))
             )
