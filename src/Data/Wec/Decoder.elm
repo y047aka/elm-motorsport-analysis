1b499804421a1f7ffbe39030dc35f74e26255942
Yoshitaka Totsuka <y047aka@gmail.com>
2025-01-01 08:56:12 +0900

    chore: move `lapDecoder` to `Fixture.Csv`

--- src/Data/Wec/Decoder.elm
+++ src/Data/Wec/Decoder.elm
@@ -1,8 +1,7 @@
-module Data.Wec.Decoder exposing (Lap, lapDecoder)
+module Data.Wec.Decoder exposing (Lap)
 
-import Csv.Decode as Decode exposing (Decoder, field, float, int, pipeline, string)
-import Motorsport.Class as Class exposing (Class)
-import Motorsport.Duration as Duration exposing (Duration)
+import Motorsport.Class exposing (Class)
+import Motorsport.Duration exposing (Duration)
 
 
 
@@ -41,40 +40,3 @@ type alias RaceClock =
 
 
 -- DECODER
-
-
-lapDecoder : Decoder Lap
-lapDecoder =
-    Decode.into Lap
-        |> pipeline (field "NUMBER" string)
-        |> pipeline (field "DRIVER_NUMBER" int)
-        |> pipeline (field "LAP_NUMBER" int)
-        |> pipeline (field "LAP_TIME" raceClockDecoder)
-        |> pipeline (field "LAP_IMPROVEMENT" int)
-        |> pipeline (field "CROSSING_FINISH_LINE_IN_PIT" string)
-        |> pipeline (field "S1" <| Decode.blank raceClockDecoder)
-        |> pipeline (field "S1_IMPROVEMENT" int)
-        |> pipeline (field "S2" <| Decode.blank raceClockDecoder)
-        |> pipeline (field "S2_IMPROVEMENT" int)
-        |> pipeline (field "S3" <| Decode.blank raceClockDecoder)
-        |> pipeline (field "S3_IMPROVEMENT" int)
-        |> pipeline (field "KPH" float)
-        |> pipeline (field "ELAPSED" raceClockDecoder)
-        |> pipeline (field "HOUR" raceClockDecoder)
-        |> pipeline (field "TOP_SPEED" <| Decode.blank Decode.float)
-        |> pipeline (field "DRIVER_NAME" string)
-        |> pipeline (field "PIT_TIME" <| Decode.blank raceClockDecoder)
-        |> pipeline (field "CLASS" classDecoder)
-        |> pipeline (field "GROUP" string)
-        |> pipeline (field "TEAM" string)
-        |> pipeline (field "MANUFACTURER" string)
-
-
-raceClockDecoder : Decoder RaceClock
-raceClockDecoder =
-    string |> Decode.andThen (Duration.fromString >> Decode.fromMaybe "Expected a RaceClock")
-
-
-classDecoder : Decoder Class
-classDecoder =
-    string |> Decode.andThen (Class.fromString >> Decode.fromMaybe "Expected a Class")
