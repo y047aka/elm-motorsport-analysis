58b29e973550e2a69639f311c092b563852ea0f3
Yoshitaka Totsuka <y047aka@gmail.com>
2025-01-11 10:48:28 +0900

    test: CLIでの前処理と、それを元にしたJSONデコードで同じ結果が得られることを確認

--- cli/tests/DecoderTest.elm
+++ cli/tests/DecoderTest.elm
@@ -1,7 +1,9 @@
 module DecoderTest exposing (suite)
 
+import Data.Wec.Preprocess
 import Expect
 import Fixture.Csv
+import Fixture.Json
 import Fixture.Json.Laps
 import Test exposing (Test, describe, test)
 
@@ -13,4 +15,9 @@ suite =
             \_ ->
                 Fixture.Json.Laps.jsonDecoded
                     |> Expect.equal Fixture.Csv.csvDecoded
+        , test "jsonDecoded and preprocessed" <|
+            \_ ->
+                -- CLIでの前処理と、それを元にしたJSONデコードで同じ結果が得られることを確認
+                (Fixture.Json.jsonDecoded |> .preprocessed)
+                    |> Expect.equal (Data.Wec.Preprocess.preprocess { laps = Fixture.Csv.csvDecoded })
         ]
