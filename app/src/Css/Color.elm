4eb9d97516e0528ff9d9aa1f1e79966572e90f0b
Yoshitaka Totsuka <y047aka@users.noreply.github.com>
2025-05-06 22:41:57 +0900

    env: Dev Container 2 (#19)

--- app/src/Css/Color.elm
+++ app/src/Css/Color.elm
@@ -4,7 +4,15 @@ import Css
 
 
 type Color
-    = ColorValue Css.Color
+    = ColorValue
+        -- Css.Colorと同じだが、lamderaの型推論が上手くいかないため、プロティを直接記述している
+        { red : Int
+        , green : Int
+        , blue : Int
+        , alpha : Float
+        , value : String
+        , color : Css.Compatible
+        }
     | CurrentColor
     | Transparent
 
