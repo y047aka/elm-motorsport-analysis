c9a651360a07547dc989e4484d358f9066749353
Yoshitaka Totsuka <y047aka@gmail.com>
2025-07-02 01:26:25 +0000

    refactor: unexpose `updateCars`

--- package/src/Motorsport/RaceControl.elm
+++ package/src/Motorsport/RaceControl.elm
@@ -1,4 +1,4 @@
-module Motorsport.RaceControl exposing (CarEventType(..), Event, EventType(..), Model, Msg(..), applyEvents, calcEvents, empty, init, update, updateCars)
+module Motorsport.RaceControl exposing (CarEventType(..), Event, EventType(..), Model, Msg(..), applyEvents, calcEvents, empty, init, update)
 
 import List.Extra
 import Motorsport.Car as Car exposing (Car, CarNumber, Status(..))
