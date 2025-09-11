ca04ea70668dce56914a5579c28aa9b7bd214ce7
Yoshitaka Totsuka <y047aka@gmail.com>
2025-09-11 07:36:39 +0000

    chore: remove overflowY property from HalfModal component
    - Eliminated the overflowY property to streamline the styling of the HalfModal, enhancing the overall UI experience.

--- app/src/UI/HalfModal.elm
+++ app/src/UI/HalfModal.elm
@@ -53,7 +53,6 @@ view props =
             , property "border-radius" "16px 16px 0 0"
             , property "box-shadow" "0 -4px 20px hsl(0 0% 0% / 0.1)"
             , property "z-index" "1"
-            , overflowY scroll
             , property "transform" modalTransform
             , property "transition" modalTransition
             , Css.Global.children
