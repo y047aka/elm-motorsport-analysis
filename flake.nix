ca9d48dab14f6d46968daa0900fad004311699c4
Yoshitaka Totsuka <y047aka@users.noreply.github.com>
2025-11-23 19:35:40 +0900

    env: introduce Nix (#66)

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05-darwin";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              nushell
+              nodejs_24
+              rustc
+              cargo
+            ];
+
+            shellHook = ''
+              # Only launch nushell if running interactively
+              if [ -t 0 ]; then
+                exec nu
+              fi
+            '';
+          };
+      });
+}
