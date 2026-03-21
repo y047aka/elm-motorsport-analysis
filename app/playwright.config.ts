b02e4d538090959e75980babdd2e20e98ebdf046
Yoshitaka Totsuka <y047aka@gmail.com>
2026-03-21 21:47:51 +0900

    tmp: --disable-font-subpixel-positioning

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     launchOptions: {
       args: [
-        '--enable-font-antialiasing',
+        '--disable-font-subpixel-positioning',
       ],
     },
   },
