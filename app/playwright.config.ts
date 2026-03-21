7cb73a70b130a46235ffca9aa8fe1901ee146425
Yoshitaka Totsuka <y047aka@gmail.com>
2026-03-21 21:56:58 +0900

    tmp: --disable-lcd-text

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     launchOptions: {
       args: [
-        '--disable-font-subpixel-positioning',
+        '--disable-lcd-text',
       ],
     },
   },
