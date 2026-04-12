a3d3ddce2936bbbf75d88219c311aec9acbeeddd
Yoshitaka Totsuka <y047aka@gmail.com>
2026-04-12 14:01:40 +0900

    chore: update ReviewConfig

--- app/review/src/ReviewConfig.elm
+++ app/review/src/ReviewConfig.elm
@@ -11,24 +11,67 @@ when inside the directory containing this file.
 
 -}
 
+import NoSimpleLetBody
+
+import CognitiveComplexity
+
+import NoConfusingPrefixOperator
 import NoExposingEverything
-import NoMissingSubscriptionsCall
+import NoImportingEverything
 import NoMissingTypeAnnotation
-import NoPrematureLetComputation
+
+import NoDebug.Log
+import NoDebug.TodoOrToString
+
+import NoUnapprovedLicense
+
+import NoUnoptimizedRecursion
+
+import NoConfusingPrefixOperator
 import NoRecursiveUpdate
 import NoUselessSubscriptions
+
+import NoUnused.Dependencies
+import NoUnused.Parameters
+import NoUnused.Patterns
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    [ -- jfmengels/elm-review-common
-      NoExposingEverything.rule
+    [
+      -- jfmengels/elm-review-code-style
+      NoSimpleLetBody.rule
+
+    -- jfmengels/elm-review-cognitive-complexity
+    , CognitiveComplexity.rule 15
+
+    -- jfmengels/elm-review-common
+    , NoConfusingPrefixOperator.rule
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule [ "Css" ]
     , NoMissingTypeAnnotation.rule
-    , NoPrematureLetComputation.rule
+
+    -- jfmengels/elm-review-debug
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+
+    -- jfmengels/elm-review-license
+    , NoUnapprovedLicense.rule
+        { allowed = [ "BSD-3-Clause", "MIT", "MPL-2.0" ]
+        , forbidden = [ "GPL-3.0-only", "GPL-3.0-or-later" ]
+        }
+
+    -- jfmengels/elm-review-performance
+    , NoUnoptimizedRecursion.rule (NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO")
 
     -- jfmengels/elm-review-the-elm-architecture
-    , NoMissingSubscriptionsCall.rule
+    , NoConfusingPrefixOperator.rule
     , NoRecursiveUpdate.rule
     , NoUselessSubscriptions.rule
+
+    -- jfmengels/elm-review-unused
+    -- , NoUnused.Dependencies.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
     ]
