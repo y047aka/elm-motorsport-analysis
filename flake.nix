fced38e0847f63ea1190b27c7dad9377084e4ac9
Yoshitaka Totsuka <y047aka@gmail.com>
2026-07-18 02:04:01 +0900

    chore: upgrade Flix to 0.75.1
    - Rename Util.Json to Util.Json.Encode and drop empty Util module to
      avoid collision with the new stdlib Util / Util.Json modules
    - Update GetOpt references to Util.GetOpt (moved in stdlib)

--- flake.nix
+++ flake.nix
@@ -74,10 +74,10 @@
           };
 
         flix = pkgs.flix.overrideAttrs (old: rec {
-          version = "0.72.0";
+          version = "0.75.1";
           src = pkgs.fetchurl {
             url = "https://github.com/flix/flix/releases/download/v${version}/flix.jar";
-            hash = "sha256-87WDphvCBJf5M46NtKGCTEu6k0g6SF/yttmRrEA8Nis=";
+            hash = "sha256-4xd3AK6tiiKkLJEOc7+4oyb+/bq04+rq9tVcMopr2Tg=";
           };
         });
 
