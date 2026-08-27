5c38757386d022d412b5edbe9395e66e43cb1cc0
Yoshitaka Totsuka <y047aka@gmail.com>
2026-08-27 21:49:33 +0900

    refactor: migrate CarSelector off elm-css
    Same shape as LiveStandings: static layout moves to Tailwind classes,
    the selected chip's manufacturer-color border/background use
    Html.Styled.Attributes.style (plain CSS properties), and the class
    badge's ::before marker uses the raw-style-attribute + static
    before:[...] class workaround for its dynamic color.
    
    class was already taken as the car-class parameter name in this
    module, so Html.Styled.Attributes is imported qualified here instead
    of exposing class unqualified.

--- package/src/Motorsport/Widget/Compare/CarSelector.elm
+++ package/src/Motorsport/Widget/Compare/CarSelector.elm
@@ -6,9 +6,8 @@ module Motorsport.Widget.Compare.CarSelector exposing (carSelector, classBadge)
 
 -}
 
-import Css exposing (num, opacity, property)
 import Html.Styled exposing (Html, button, div, text)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled.Attributes as Attributes exposing (style)
 import Html.Styled.Events exposing (onClick)
 import Motorsport.Race.Snapshot as Snapshot exposing (CarAt, Snapshot)
 import Motorsport.Wec.Class as Class exposing (Class)
@@ -24,12 +23,7 @@ carSelector onToggleCar standings class selectedCarNumbers =
             Snapshot.inClass class standings
     in
     div
-        [ css
-            [ property "display" "flex"
-            , property "flex-wrap" "wrap"
-            , property "gap" "6px"
-            ]
-        ]
+        [ Attributes.class "flex flex-wrap gap-1.5" ]
         (List.map
             (\item ->
                 carSelectorChip onToggleCar
@@ -47,54 +41,25 @@ carSelectorChip onToggleCar isSelected item =
             item.metadata.manufacturer.color
     in
     button
-        [ onClick (onToggleCar item.metadata.carNumber)
-        , css
-            [ property "display" "flex"
-            , property "align-items" "center"
-            , property "column-gap" "4px"
-            , property "padding" "2px 8px"
-            , property "border-radius" "9999px"
-            , property "font-size" "11px"
-            , property "font-weight" "700"
-            , property "font-variant-numeric" "tabular-nums"
-            , property "cursor" "pointer"
-            , property "color" "inherit"
-            , property "font-family" "inherit"
-            , if isSelected then
-                Css.batch
-                    [ property "border" ("1px solid " ++ manufacturerColor)
-                    , property "background-color" ("oklch(from " ++ manufacturerColor ++ " l c h / 0.3)")
+        ([ onClick (onToggleCar item.metadata.carNumber)
+         , Attributes.class "flex items-center gap-x-1 py-0.5 px-2 rounded-full text-[11px] font-bold tabular-nums cursor-pointer text-inherit [font-family:inherit]"
+         ]
+            ++ (if isSelected then
+                    [ style "border" ("1px solid " ++ manufacturerColor)
+                    , style "background-color" ("oklch(from " ++ manufacturerColor ++ " l c h / 0.3)")
                     ]
 
-              else
-                Css.batch
-                    [ property "border" "1px solid hsl(0 0% 100% / 0.2)"
-                    , property "background-color" "transparent"
-                    , opacity (num 0.7)
-                    ]
-            ]
-        ]
+                else
+                    [ Attributes.class "border border-[hsl(0_0%_100%/0.2)] bg-transparent opacity-70" ]
+               )
+        )
         [ text ("#" ++ item.metadata.carNumber) ]
 
 
 classBadge : Class -> Html msg
 classBadge class =
     div
-        [ css
-            [ property "display" "flex"
-            , property "align-items" "center"
-            , property "column-gap" "4px"
-            , property "font-size" "11px"
-            , property "font-weight" "700"
-            , property "white-space" "nowrap"
-            , Css.before
-                [ property "display" "block"
-                , property "content" (Css.qt "")
-                , property "width" "0.2em"
-                , property "height" "1em"
-                , property "border-radius" "2px"
-                , Css.property "background-color" (Class.toColor class).value
-                ]
-            ]
+        [ Attributes.class "flex items-center gap-x-1 text-[11px] font-bold whitespace-nowrap before:block before:content-[''] before:w-[0.2em] before:h-[1em] before:rounded-[2px] before:[background-color:var(--class-color)]"
+        , Attributes.attribute "style" ("--class-color: " ++ (Class.toColor class).value ++ ";")
         ]
         [ text (Class.toString class) ]
